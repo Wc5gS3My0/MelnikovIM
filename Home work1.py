f78838f9b95673038c8627d11fe6d5ea28806895
Wc5gS3My0 <ivanmelnikov378@gmail.com>
2024-06-29 20:35:15 +0300

    Add files via upload

---
+++ Home work1.py
@@ -0,0 +1,27 @@
+# длинна слова
+m = 'nickelback'
+print(len(m))
+
+# сумма разности
+first= 9
+second= 5
+print( first+second)
+print(first-second)
+
+# среднее арифметическое
+first, second, third= 12, 8, 4
+print((first+second+third)//3)
+
+# простые строчки
+j='вторник'
+t='понедельник'
+print(t,',',j, sep='')
+
+# сложная формула
+a=8
+b=2
+c=3
+print((a*b)+(a*c))
+f=40
+print((40**3)//2)
+
