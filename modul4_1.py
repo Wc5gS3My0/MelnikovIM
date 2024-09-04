27701af74ae1c7fcfc7a62437b748dea06a5b408
Wc5gS3My0 <ivanmelnikov378@gmail.com>
2024-09-04 22:31:32 +0300

    Add files via upload

---
+++ modul4_1.py
@@ -0,0 +1,14 @@
+import fake_math as f_m
+import true_match as t_m
+
+fake_math=f_m
+true_match=t_m
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
