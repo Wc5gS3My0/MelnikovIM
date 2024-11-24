b0bd6a466d9e09e5970e7b6e539796cdf4dc0aae
Wc5gS3My0 <ivanmelnikov378@gmail.com>
2024-11-24 09:26:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+print('1st program')
+print(9**0.5*5)
+
+print('2st program')
+print(99.9 > 99.8 and 1000 !=1000.1)
+
+print('3st program' )
+print((2 * 2 +2) == (2 * (2 + 2)))
+
+print(' 4st program')
+'123.456'
+print(int (123.456 * 10) % 10)
