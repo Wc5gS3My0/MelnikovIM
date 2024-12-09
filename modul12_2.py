2299cb7647434973adfe3d9621940446e5b4a0ee
Wc5gS3My0 <ivanmelnikov378@gmail.com>
2024-12-09 13:13:01 +0300

    Add files via upload

---
+++ modul12_2.py
@@ -0,0 +1,62 @@
+import unittest
+import runner_and_tournament as rt
+
+
+class TournamentTest(unittest.TestCase):
+    all_results = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+
+    def setUp(self):
+        self.run1 = rt.Runner('Усэйн', 10)
+        self.run2 = rt.Runner('Андрей', 9)
+        self.run3 = rt.Runner('Ник', 3)
+        self.run4 = rt.Runner('Алекс', 5)
+
+
+    @classmethod
+    def tearDownClass(cls):
+
+        result = {}
+        
+        for testkey, testval in cls.all_results.items():
+
+            print(f'TEST: {testkey}')
+
+            for key, val in testval.items():
+
+                result[key] = str(val.name)
+
+            print(result)
+
+
+    def testrun_1(self):
+
+        run_1 = rt.Tournament(90, self.run1, self.run3)
+
+        finish = run_1.start()
+
+        self.assertTrue(list(finish.values())[-1].name == str(self.run3))
+
+        self.all_results[f'Результат {self.run1} и {self.run3}'] = finish
+
+
+    def testrun_2(self):
+        run_1 = rt.Tournament(90, self.run2, self.run3)
+        finish = run_1.start()
+        self.assertTrue(list(finish.values())[-1].name == str(self.run3))
+        self.all_results[f'Результат {self.run2} и {self.run3}'] = finish
+
+
+    def testrun_3(self):
+        run_1 = rt.Tournament(90, self.run1, self.run2, self.run3)
+        finish = run_1.start()
+        self.assertTrue(list(finish.values())[-1].name == str(self.run3))
+        self.all_results[f'Результат {self.run1}, {self.run2} и {self.run3}'] = finish
+
+
+if __name__ == "__main__":
+    unittest.main()
